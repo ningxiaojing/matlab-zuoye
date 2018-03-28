cd9d176082003f406cbe37612e6fc937127e43e2
ningxiaojing <ningxiaojing@users.noreply.github.com>
2018-03-28 16:09:57 +0800

    zuoye2__xiugai
    作业第二次修改，正确提取花朵

--- zuoye2/Untitled.m
+++ zuoye2/Untitled.m
@@ -3,17 +3,18 @@ clear all;
 clc;
 img=imread('flowers.png');
 subplot(1,3,1);imshow(img);
-huidu=rgb2gray(img);        %图像灰度化
+huidu=rgb2gray(img);               %图像灰度化
 subplot(1,3,2);imshow(huidu);
-thresh=graythresh(huidu);       %自动确定二值化阈值  
-img_bw=im2bw(huidu,thresh);      %对图像二值化 
+% thresh=graythresh(huidu);        %自动确定二值化阈值  
+img_bw=im2bw(huidu,0.1);          %对图像二值化，二值化的阈值是手动设置为0.1了
 subplot(1,3,3);imshow(img_bw);
-se=strel('disk',9);
-kai=imopen(img_bw,se);
-figure,subplot(1,3,1);imshow(kai);
-guan=imclose(kai,se);
-subplot(1,3,2);imshow(guan);
-% K=bwlabel(guan);
-% rgb=labelrgb(K);
-% img_color=img.*img_bw;
-% subplot(1,3,3);imshow(img_color);
+se=strel('disk',9);               %创建一个平坦的圆形结构元素，半径为9
+kai=imopen(img_bw,se);            %开操作，即先腐蚀后膨胀
+figure,subplot(1,2,1);imshow(kai);
+guan=imclose(kai,se);             %闭操作，即先膨胀后腐蚀
+subplot(1,2,2);imshow(guan);
+[m,n,k] = size(img);
+ind = reshape([guan,guan,guan],m,n,k); % ind是把三张二维的guan图像组合成一个三维的图像的结果
+img_color = img .* uint8(ind);
+figure,imshow(img_color);
+imwrite(img_color,'flowers2.png');%保存图片
